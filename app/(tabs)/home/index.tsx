33c3f11052226fe1360a4f3236e5e2fc82a98f3f
dkueng <davidwichtig@gmx.at>
2024-06-23 17:32:02 +0200

    Added tamagui for styling

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { View, Heading } from 'tamagui'
 
 export default function home() {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <View style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
+      <Heading size={'$10'} color={'violet'}>
+        TAMAGUI
+      </Heading>
       <Link href='/home/settings'>Push settings</Link>
     </View>
   )
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,57 @@
+import { useFonts, Inter_400Regular, Inter_900Black } from "@expo-google-fonts/inter";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+import { useColorScheme } from "@/components/useColorScheme";
+import { TamaguiProvider } from "tamagui";
+import tamaguiConfig from "@/tamagui.config";
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Inter: Inter_400Regular,
+    InterBold: Inter_900Black,
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}} />
-    </Stack>
+    <TamaguiProvider
+      config={tamaguiConfig}
+      defaultTheme={colorScheme as string}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
